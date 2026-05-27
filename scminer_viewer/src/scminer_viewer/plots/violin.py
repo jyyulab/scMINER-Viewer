11496a2a6b7f062f61118b81cb78238eff96053f
Khoa Tran <trannguyenanhkhoa05122003@gmail.com>
2026-05-27 14:43:24 -0500

    Fix violin plot box rendering and border styling

--- scminer_viewer/src/scminer_viewer/plots/violin.py
+++ scminer_viewer/src/scminer_viewer/plots/violin.py
@@ -10,6 +10,14 @@ from ..data import Study
 from ._common import cells_mask, cluster_color_map, empty_figure
 
 
+def _darken(hex_color: str, factor: float = 0.55) -> str:
+    h = hex_color.lstrip("#")
+    r, g, b = (int(h[i:i + 2], 16) for i in (0, 2, 4))
+    return "#{:02x}{:02x}{:02x}".format(
+        int(r * factor), int(g * factor), int(b * factor)
+    )
+
+
 def violin_plot(
     study: Study,
     gene: str,
@@ -30,16 +38,24 @@ def violin_plot(
     fig = go.Figure()
     for ct in df["cellType"].unique():
         sub = df.loc[df["cellType"] == ct, "value"]
+        color = colors.get(ct, "#888888")
         fig.add_trace(
             go.Violin(
+                x=[ct] * len(sub),
                 y=sub,
                 name=ct,
-                box_visible=True,
-                meanline_visible=True,
+                box=dict(
+                    visible=True,
+                    width=0.25,
+                    fillcolor=color,
+                    line=dict(color=_darken(color), width=1),
+                ),
+                meanline=dict(visible=True, color=_darken(color), width=1),
                 points="outliers",
-                line_color=colors.get(ct, "#888888"),
-                fillcolor=colors.get(ct, "#888888"),
+                line=dict(color=_darken(color)),
+                fillcolor=color,
                 opacity=0.6,
+                showlegend=False,
             )
         )
     fig.update_layout(
